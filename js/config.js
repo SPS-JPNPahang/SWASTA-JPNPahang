3364b2c8d34d8644dec11053c16494c349de7bc9
SPS-JPNPahang <sektorpengurusansekolahpahang@gmail.com>
2025-11-22 08:01:26 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 /* ===== CONFIGURATION ===== */
 /* GANTI URL ini dengan Web App URL anda selepas deploy Apps Script */
 
-const GAS_WEBAPP_BASE = 'https://script.google.com/macros/s/AKfycbwevBVgJwvbpk_Esm9Y_89YswFHJEo7huZx3sRwy5SOe511yEOeTWWnsalMiHuiZ-yu/exec';
+const GAS_WEBAPP_BASE = 'https://script.google.com/macros/s/AKfycbwafWP1mQirWNqEQFoCNY7ISJI5BNqkQmfTx5ZD7nHaw2DfIxRjfvF0XB9MFdU2RJo4/exec';
 
 // Helper function to build GET URLs
 function buildGET(action, params = {}) {
@@ -18,4 +18,5 @@ const GAS_POST = GAS_WEBAPP_BASE;
 
 /* ===== FILE SIZE LIMITS ===== */
 const MAX_FILE_SIZE = 5 * 1024 * 1024; // 5MB per file
+
 const ACCEPTED_FILE_TYPE = 'application/pdf';
